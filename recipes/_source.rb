69fec94bf33f9704532058ae37164f4d7e2fab9e
yokoyama koichi <sehr.gluck@gmail.com>
2019-09-28 23:44:17 +0900

    update download URL

--- recipes/_source.rb
+++ recipes/_source.rb
@@ -9,7 +9,7 @@ cache_dir = Chef::Config[:file_cache_path]
 cmake_version = node["cmake"]["source"]["version"]
 
 remote_file "#{cache_dir}/cmake-#{cmake_version}.tar.gz" do
-  source "http://www.cmake.org/files/v#{cmake_version[/^\d\.\d/, 0]}/cmake-#{cmake_version}.tar.gz" # rubocop:disable LineLength
+  source "https://github.com/Kitware/CMake/releases/download/v#{cmake_version}/cmake-#{cmake_version}.tar.gz"
   checksum node["cmake"]["source"]["checksum"]
   notifies :run, "execute[unpack cmake]"
 end
